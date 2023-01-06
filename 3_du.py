cc218ccae12aa169a816288c64a4f6b380c1ba4d
Kristýna Antošová <antosovkr@natur.cuni.cz>
2023-01-06 23:39:22 +0100

    Druhý commit - načtení geojson souborů

--- 3_du.py
+++ 3_du.py
@@ -1,1 +1,10 @@
-#zkouška githubu
+import json
+#from pyproj import Transformer
+#Přečtení geojson souborů
+with open("adresy.geojson", encoding="utf-8") as f:
+    adresy = json.load(f)
+    print(adresy)
+
+with open("kontejnery.geojson", encoding="utf-8") as f:
+    kontejnery = json.load(f)
+    print(kontejnery)
