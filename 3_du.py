027ad797011d0d1780f81cc206605d7bd152e164
Kristýna Antošová <antosovkr@natur.cuni.cz>
2023-01-07 16:22:19 +0100

    Pátý commit - Výpočet nejmenších vzdáleností

--- 3_du.py
+++ 3_du.py
@@ -1,10 +1,10 @@
-import json
+import json, math
 from pyproj import Transformer
 
+#výpočet vzdálenosti
 def vypocet_vzdalenosti (x_k, y_k, x_a, y_a):
     hodnota = (x_k - x_a)**2 + (y_k - y_a)**2
     return math.sqrt(hodnota)
-
 
 #Přečtení geojson souborů
 with open("adresy.geojson", encoding="utf-8") as f:
@@ -14,8 +14,6 @@ with open("adresy.geojson", encoding="utf-8") as f:
 with open("kontejnery.geojson", encoding="utf-8") as f:
     kontejnery = json.load(f)
     print(kontejnery)
-
-
 
 #převod z WGS na JTSK
 wgs2jtsk = Transformer.from_crs(4326,5514,always_xy=True)
@@ -37,3 +35,21 @@ for f in kontejnery['features']:
     jtsk_adresy = x_k,y_k 
     kontejnery_souradnice_jtsk.append(jtsk_adresy)
 print (kontejnery_souradnice_jtsk)
+
+
+print ("toto je seznam nejm vzd")
+seznam_nejmensi_vzdalenosti = []
+# funkce na výpočet nejmenších vzdaleností uživatelem zvolených adres ke kontejnerům v Praze  
+def vypocet_nejmensich_vzdalenosti(adresy):
+    for adresa in adresy:
+        nejmensi_vzdalenost = 10000
+        for kontejner in kontejnery_souradnice_jtsk:
+            vzdalenost = vypocet_vzdalenosti(adresa[0], adresa[1], kontejner[0], kontejner[1])
+            if (vzdalenost <= nejmensi_vzdalenost): 
+                nejmensi_vzdalenost = vzdalenost
+        if (nejmensi_vzdalenost == 10000): 
+            print("nenalezeno")
+        else: seznam_nejmensi_vzdalenosti.append(nejmensi_vzdalenost)
+vypocet_nejmensich_vzdalenosti(adresy_souradnice_jtsk)
+
+print (seznam_nejmensi_vzdalenosti)
